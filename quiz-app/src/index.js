57faa14e0a0666a198f1588de5bd936dbc6fde9e
angrycatjenny <angrycatjenny@gmail.com>
2021-06-28 22:35:54 +0900

    index.js 라우터 적용, 리덕스 적용

--- quiz-app/src/index.js
+++ quiz-app/src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "../src/views/Home/Home.js";
+import Quiz from "../src/views/Quiz/Quiz.js";
+import Analysis from "../src/views/Analysis/Analysis.js";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "../src/redux/store.js";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/quiz" component={Quiz} />
+          <Route path="/analysis" component={Analysis} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
