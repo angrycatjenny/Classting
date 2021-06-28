68aab5a532bd9a0eb596375992283b671631080e
angrycatjenny <angrycatjenny@gmail.com>
2021-06-28 22:14:30 +0900

    index.js에 redux 적용

--- quiz-app/src/views/Quiz/Quiz.js
+++ quiz-app/src/views/Quiz/Quiz.js
@@ -5,16 +5,20 @@ import Home from "../src/views/Home/Home.js";
 import Quiz from "../src/views/Quiz/Quiz.js";
 import Analysis from "../src/views/Analysis/Analysis.js";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "../src/redux/store.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/quiz" component={Quiz} />
-        <Route path="/analysis" component={Analysis} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/quiz" component={Quiz} />
+          <Route path="/analysis" component={Analysis} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
