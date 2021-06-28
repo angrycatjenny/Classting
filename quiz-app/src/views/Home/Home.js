f6a0e28349cac6262c17ccccc0456f1ef2e8f3ad
angrycatjenny <angrycatjenny@gmail.com>
2021-06-28 22:18:30 +0900

    퀴즈 시작 버튼 클릭시 요청 및 데이터 처리

--- quiz-app/src/views/Home/Home.js
+++ quiz-app/src/views/Home/Home.js
@@ -10,13 +10,42 @@ const Home = ({ history }) => {
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useDispatch();
 
+  const startQuiz = () => {
+    setIsLoading(true);
+    api
+      .getQuizData({ amount: 10 })
+      .then((res) => {
+        const results = res.data.results;
+        let options = [];
+        results.map((result) => {
+          const selectArr = [
+            ...result.incorrect_answers,
+            result.correct_answer,
+          ];
+          options = [...options, randomItem(selectArr)];
+        });
+        dispatch(setQuestionsRequestAction({ questions: results, options }));
+        setIsLoading(false);
+        history.push("/quiz");
+      })
+      .catch((e) => {
+        console.log(e, "e");
+      });
+  };
+
+  const randomItem = (options) => {
+    return options.sort(function (a, b) {
+      return 0.5 - Math.random();
+    });
+  };
+
   return (
     <div className="center">
       {isLoading ? (
         <Loading />
       ) : (
         <div className="center">
-          <button className="quizButton">
+          <button className="quizButton" onClick={startQuiz}>
             <span className="shadow"></span>
             <span className="edge"></span>
             <span className="phrase">Let's Quiz!</span>
