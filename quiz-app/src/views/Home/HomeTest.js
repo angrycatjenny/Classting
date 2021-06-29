2bcb7868cbb9b7ea57bb1d90fcafcd038b5bc569
angrycatjenny <angrycatjenny@gmail.com>
2021-06-30 00:28:44 +0900

    Home.js 테스트 수정

--- quiz-app/src/views/Home/HomeTest.js
+++ quiz-app/src/views/Home/HomeTest.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import Home from "./Home.js";
 
 describe("<Home />", () => {
   it("matches snapshot", () => {
-    const wrapper = mount(<Home />);
+    const wrapper = shallow(<Home />);
+    wrapper.setState({
+      isLoading: false,
+    });
+    expect(wrapper.find("div").length).toBe(2);
+    expect(wrapper.find("button").length).toBe(1);
+    expect(wrapper.find("span").length).toBe(3);
     expect(wrapper).toMatchSnapshot();
   });
   it("start", () => {
-    const wrapper = mount(<Home />);
+    const wrapper = shallow(<Home />);
     let quizButton = wrapper.findWhere((node) => node.type() === "button");
     quizButton.simulate("click");
   });
